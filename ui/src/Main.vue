3b819cb3fbd04f5805e00156942ae642f83a9b3a
pbca26 <pbca26@gmail.com>
2022-02-06 20:48:31 +0300

    add main comp

---
+++ ui/src/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" width="50px" style="margin-bottom: 40px">
+    <div class="container">
+      content here
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
