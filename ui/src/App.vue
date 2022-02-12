a0ca36c2cec46fb6a53817352442aa68988b277a
pbca26 <pbca26@gmail.com>
2022-02-12 20:02:48 +0300

    (ui) add router, vue-bootstrap

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,3 +1,5 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <router-view /> 
+  </div>
 </template>
--- ui/src/main.js
+++ ui/src/main.js
@@ -1,9 +1,28 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Main from './Main.vue';
+import { BootstrapVue } from 'bootstrap-vue';
+import VueRouter from 'vue-router';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false;
 
+[VueRouter, BootstrapVue].forEach((x) => Vue.use(x));
+
+const routes = [
+  {
+    path: "",
+    component: Main,
+  }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: "history",
+});
+
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount("#app");
