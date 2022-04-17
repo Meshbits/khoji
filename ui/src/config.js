3be918c20cceb17ed11a9085deac7d107b2c0184
pbca26 <pbca26@gmail.com>
2022-04-17 16:31:22 +0300

    (ui) add page titles

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,2 +1,4 @@
-export const apiURL = window.location+'api/v1';
+export const apiURL = window.location + 'api/v1';
+//export const apiURL = 'https://dev.khoji.io/api/v1';
 export const isMock = false;
+export const appTitle = 'Khoji Explorer';
